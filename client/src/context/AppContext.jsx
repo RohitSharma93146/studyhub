099c0664f373932ebcaf4a806ebe774eff7922bf
RohitSharma93146 <rohitsharma93146@gmail.com>
2025-06-13 13:34:24 +0530

    Refactor application structure and enhance UI components with new features

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,36 @@
-import {createContext} from 'react';
+import {createContext, useEffect, useState} from 'react';
+import { dummyCourses } from '../../assets/assets';
+import { useNavigate } from 'react-router-dom';
+
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+
+    const currency = import.meta.env.VITE_CURRENCY;
+    const navigate = useNavigate();
+    const [allCourses, setAllCourses] = useState([]);
+
+    // Fetch all courses from the server
+    const fetchAllCourses = async () => {
+        setAllCourses(dummyCourses)
+    }
+
+    const calculateRating = (course)=>{
+        if(course.courseRatings.length === 0) return 0;
+
+        let totalRating = 0;
+        course.courseRatings.forEach((rating) => {
+            totalRating += rating.rating;
+        });
+        return totalRating / course.courseRatings.length;
+    }
+    useEffect(() => {
+        fetchAllCourses();
+    }, [])  
+
     const value = {
+        currency, allCourses, navigate, calculateRating
     }
     
     return(
